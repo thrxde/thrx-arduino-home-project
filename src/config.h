0c353db622fdc43522c1ff5d21c2377ef5b79b1b
thrx.de <3653003+thrxde@users.noreply.github.com>
2022-04-09 13:01:20 +0200

    introduce private config (#4)

---
+++ src/config.h
@@ -0,0 +1,20 @@
+#pragma once
+
+// check (required) parameters passed from the ini
+// create your own private_config.ini with the data. See private_config.template.ini
+//#ifndef SERIAL_BAUD
+//#error Need to pass SERIAL_BAUD
+//#endif
+
+// MQTT Username
+#ifndef MQTT_USER
+//#define MQTT_USER "username"
+#error Need to pass MQTT_USER
+#endif
+
+// MQTT Password
+#ifndef MQTT_PASS
+//#define MQTT_PASS "password"                        
+#error Need to pass MQTT_PASS
+#endif
+
