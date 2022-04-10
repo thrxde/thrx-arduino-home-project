53d8f13945105b8c0329756f8e0d17eab8846d9a
thrx.de <coder@thrx.de>
2022-04-10 22:16:20 +0200

    adds error when mqtt is not connected

--- src/mqtthandler.cpp
+++ src/mqtthandler.cpp
@@ -1,16 +1,21 @@
 #include "mqtthandler.h"
 
 void MqttHandler::publish(const char* topic, const char* payload){
-	Serial.print("publish -- topic: ");
-	Serial.print(strlen(topic));
-	Serial.print(" -- payload: ");
-	Serial.print(  strlen(payload));
-	Serial.print(" -- lenghth: ");
-	Serial.print( 5 + 2 + strlen(topic) + strlen(payload));
-	Serial.print(" -- ");
-	Serial.print(topic);
-	Serial.print(" -- transmited: ");
-	Serial.print(mqttClient.publish(topic,payload));
-	delay(10);
-	Serial.println(" -- OK");
+	if (mqttClient.connected()) {
+		Serial.print("publish -- topic: ");
+		Serial.print(topic);
+		Serial.print(" -- payload: ");
+		Serial.print(payload);
+		Serial.print(" -- lenghth: ");
+		Serial.print( 5 + 2 + strlen(topic) + strlen(payload));
+		Serial.print(" -- transmited: ");
+		Serial.print(mqttClient.publish(topic,payload));
+		Serial.println(" -- OK");
+	} else {
+		Serial.print("publish -- topic: ");
+		Serial.print(topic);
+		Serial.print(" -- payload: ");
+		Serial.print(payload);
+		Serial.println(" -- ERROR: MQTT not connected .... ");
+	}	
 }
